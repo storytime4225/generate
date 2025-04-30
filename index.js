6202e146b47af1775a2222fc2480da2647613b17
Henry Heleine <henryheleine86@gmail.com>
2025-04-30 16:13:44 -0400

    update for more logging

--- index.js
+++ index.js
@@ -26,8 +26,10 @@ app.get("/health", function(req,res) {
 app.post("/data", (req, res) => {
     processText().then(response => {
         let cleaned = response.replace(/\n/g, "").replace(/\"/g, "/\"")
+        let output = "{ \"content\": \"" + cleaned + "\" }"
+        console.log(output)
         res.writeHead(200, {"Content-Type":"application/json"})
-        res.end("{ \"content\": \"" + cleaned + "\" }")
+        res.end(output)
     })
 })
 
